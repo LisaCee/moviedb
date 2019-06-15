a9e6fef38e5aa78297e5326e2cca40f32a84bcf8
LisaCee <lisaceedesign@gmail.com>
2019-06-15 16:54:30 -0700

    display successfully renders movies 2-20, jumbostron movie 1

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const Display = props => {
+    let others;
+    if (props.movie.results) {
+        others = props.movie.results.slice(1);
+    }
+    
     return (
         <div>
-            {props.movie.results && props.movie.results.map((movie, id) => {
+            {props.movie.results && others.map((movie, id) => {
                 let movie_poster = movie.poster_path.toString();
-                let imageURL="https://image.tmdb.org/t/p/w500" + movie_poster;
+                let imageURL = "https://image.tmdb.org/t/p/w500" + movie_poster;
                 return (
                     <div key={id}>
                         <h1 key={id}>{movie.title}</h1>
-                        
-                        <img src= {imageURL} alt="movie poster"/>
+
+                        <img src={imageURL} alt="movie poster" />
                     </div>
                 )
             })}
--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 const Jumbotron = props => {
-    console.log(props.one.results? props.one.results[0] : null)
+    let imageURL;
+    if (props.one.results) {
+        let movie_poster = props.one.results[0].poster_path;
+        imageURL = "https://image.tmdb.org/t/p/original" + movie_poster;
+    }
     return (
-<div>
-            {props.one.results ? 
-    
-   ( <h1>{props.one.results[0].title}</h1>) : (<h1>Before results</h1>)
+        <div>
+
+            {props.one.results ?
+                (<div>
+                    <h1>{props.one.results[0].title}</h1>
+                    <p>{props.one.results[0].overview}</p>
+                    <img src={imageURL} alt="movie poster" />
+                </div>) : (<h1>Put something here for splash screen</h1>)
             }
-       </div> 
+        </div>
     )
 }
 
