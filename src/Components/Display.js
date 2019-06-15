6128fc1780a13b7475c8aed9e445d3efc20dce48
LisaCee <lisaceedesign@gmail.com>
2019-06-15 10:29:52 -0700

    movie posters rendering

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 const Display = props => {
     console.log('PROPS', props.movie.results)
+  
+    
+    
     return (
         <div>
             {props.movie.results && props.movie.results.map((movie, id) => {
+                let movie_poster = movie.poster_path.toString();
+                let imageURL="https://image.tmdb.org/t/p/w500" + movie_poster;
+                console.log(imageURL)
                 return (
                     <div key={id}>
-                        <p></p>
                         <h1 key={id}>{movie.title}</h1>
-                        <img src="{movie.poster_path}" alt="movie poster"/>
+                      
+                        <img src= {imageURL} alt="movie poster"/>
                     </div>
                 )
             })}
