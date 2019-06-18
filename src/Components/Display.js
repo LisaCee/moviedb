4c493367afcd5df1f3fa9e0b7f0c94205da153ff
LisaCee <lisaceedesign@gmail.com>
2019-06-17 20:27:32 -0700

    working on edge case for no poster, running into issues with  1946

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -11,31 +11,40 @@ const Display = props => {
     return (
         <Container>
             <Row>
-
                 {props.movie.results &&
                     others.map((movie, id) => {
+
+
                         let movie_poster = movie.poster_path.toString();
                         let imageURL = "https://image.tmdb.org/t/p/w342" + movie_poster;
+
                         return (
-                           
-                                <Col
-                                    sm={{ span: 10, offset: 1 }}
-                                    md={{ span: 6, offset: 0 }}
-                                    lg="4"
-                                    key={id}
-                                >
-                                    <div>
+
+                            <Col
+                                sm={{ span: 10, offset: 1 }}
+                                md={{ span: 6, offset: 0 }}
+                                lg="4"
+                                key={id}
+                            >
+                                <div>
+                                    {props.movie.results.poster_path !== null ? (
                                         <img
                                             src={imageURL}
                                             className="img-fluid poster"
                                             alt="movie poster"
                                         />
-                                        <h1 id="movie_titles" key={id}>
-                                            {movie.title}
-                                        </h1>
-                                    </div>
-                                </Col>
-                           
+                                    ) : (<img
+                                            src="../images/brian-kraus-1233556-unsplash.jpg"
+                                            className="img-fluid poster"
+                                            alt="movie poster"
+                                        />)}
+
+                                    <h1 id="movie_titles" key={id}>
+                                        {movie.title}
+                                    </h1>
+                                </div>
+                            </Col>
+
                         );
                     })}
 
