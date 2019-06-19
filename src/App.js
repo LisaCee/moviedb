51b0e4b9191788a94f65fe009ccbf03180bbed59
LisaCee <lisaceedesign@gmail.com>
2019-06-19 10:37:01 -0700

    added text with search years, new color gradient for background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Search from "./Components/Search";
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1>Movie Time Machine</h1>
-      <Search />
+      <Container><Row><Col>
+        <h1>Movie Time Machine</h1>
+        <h4>Are you ready for a blast from the past?</h4>
+        <h6>Search between 1900 through current day to discover movies through the ages. </h6>
+        <Search />
+      </Col></Row></Container>
     </div>
   );
 }
