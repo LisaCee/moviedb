7686dbfd1b646f121bd8c96f5e88a9f60fb832f7
LisaCee <lisaceedesign@gmail.com>
2019-06-21 16:34:25 -0700

    search rendering offscreen in large bps, wip

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col } from 'react-bootstrap'
 import "../styles/header.css";
 import Jumbo from "./Jumbo";
 import Splash from "./Splash"
 
-const Header = props => {
+class HeaderSearch extends Component {
+    state = {
+        searchYear: "",
+        results: {},
+        searchYearProp: "",
+      };
+      onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+      };
+      onClick = e => {
+        e.preventDefault();
+        let api_key = process.env.REACT_APP_APIKEY;
+        let searchYear = this.state.searchYear;
+        let pageNumber = 1;
+        let baseURL = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}&with_original_language=en&primary_release_year=`;
+        fetch(baseURL + searchYear)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            this.setState({ results: data });
+            this.setState({ searchYearProp: searchYear });
+            this.setState({ searchYear: "" });
+          })
+          .catch(error => {
+            console.log("ERROR", error);
+          });
+          pageNumber += 1;
+          console.log(pageNumber)
+      };
+      render() {
     return (
         <div>
             <Container className="fixed-top">
@@ -17,16 +47,32 @@ const Header = props => {
                         </Col>
                     </Row>
                     <Row>
-                        <Form inline>
+                        <div id="search_form">
+                            <form>
+                                <input
+                                    name="searchYear"
+                                    type="number"
+                                    max="2019"
+                                    min="1900"
+                                    value={this.state.searchYear}
+                                    onChange={this.onChange}
+                                />
+                                <button onClick={this.onClick}>
+                                    <i className="fas fa-search" />
+                                </button>
+                            </form>
+                        </div>
+                        {/* <Form inline>
                             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                             <Button variant="outline-success">Search</Button>
-                        </Form>
+                        </Form> */}
                     </Row>
                 </Navbar>
             </Container >
             <Splash />
         </div >
     )
+                    }
 }
 
-export default Header;
+export default HeaderSearch;
