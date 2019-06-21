a56c342a35af1c4acfbe6afddc18f88812cce3de
LisaCee <lisaceedesign@gmail.com>
2019-06-21 16:18:30 -0700

    working on layout for nav/header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,43 +1,30 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col } from 'react-bootstrap'
 import "../styles/header.css";
+import Jumbo from "./Jumbo";
+import Splash from "./Splash"
 
 const Header = props => {
     return (
         <div>
-            {/* <Container> */}
-
-
-                <Navbar bg="dark" expand="lg" fixed="top">
+            <Container className="fixed-top">
+                <Navbar bg="dark" expand="lg">
                     <Row>
                         <Col>
                             <Navbar.Brand href="#home">Movie Time Machine</Navbar.Brand>
+
+                            <p>Are you ready for a blast from the past?</p>
                         </Col>
                     </Row>
                     <Row>
-                        <Col>
-                            <p>Are you ready for a blast from the past?</p>
-                        </Col>
-                        <Col>
-                            <Form inline>
-                                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
-                        </Col>
+                        <Form inline>
+                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
                     </Row>
-                    {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="mr-auto">
-                                    <Nav.Link href="#home">Home</Nav.Link>
-                                    <Nav.Link href="#link">Link</Nav.Link>
-
-                                </Nav>
-
-                            </Navbar.Collapse> */}
                 </Navbar>
-                    {/* </Col> */}
-                {/* </Row> */}
-            {/* </Container > */}
+            </Container >
+            <Splash />
         </div >
     )
 }
