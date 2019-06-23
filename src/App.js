fabea043eb41078a08e4ae8596bfce7f2e6b4a71
LisaCee <lisaceedesign@gmail.com>
2019-06-23 15:03:04 -0700

    replaced search component with header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./App.css";
-import Search from "./Components/Search";
-import { Container, Row, Col } from "react-bootstrap";
 import Header from "./Components/Header"
 
 function App() {
